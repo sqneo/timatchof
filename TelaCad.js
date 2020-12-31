67a7cfd7415f16f8c4bfeae5b42362707cd62536
Vusmonth <Arthurmartins08@outlook.com>
2020-12-31 20:19:33 -0300

    Update TelaCad

--- TelaCad.js
+++ TelaCad.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, ActivityIndicator, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -12,109 +12,61 @@ export default function App() {
 
             <StatusBar style="light" backgroundColor='#000' />
 
-            {/* Texto foto  testetetstetse*/}
+            {/* Texto foto */}
             <Text style={{
                 fontSize: 18,
                 fontWeight: 'bold',
                 color: '#ff739a',
                 marginTop: 50,
-                marginLeft: 50,
 
             }}>Adicione pelo menos uma foto</Text>
 
-            <View style={{
-                width: '40%',
-                height: 200, backgroundColor: '#fff',
-                elevation: 8, borderRadius: 10,
-                marginTop: 110,
-                position: 'absolute',
-                zIndex: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-start',
-                marginLeft: 20,
+            <View style={styles.imageRow}>
 
-            }}>
-                <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
+                <View style={styles.imageContainer}>
+                    <TouchableOpacity>
+                        <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.imageContainer}>
+                    <TouchableOpacity>
+                        <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
+                    </TouchableOpacity>
+                </View>
+
             </View>
 
-            <View style={{
-                width: '40%',
-                height: 200, backgroundColor: '#fff',
-                elevation: 8, borderRadius: 10,
-                marginTop: 110,
-                position: 'absolute',
-                zIndex: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-start',
-                marginLeft: 200,
+            <View style={styles.imageRow}>
 
-            }}>
-                <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
-            </View>
+                <View style={styles.imageContainer}>
+                    <TouchableOpacity>
+                        <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
+                    </TouchableOpacity>
+                </View>
 
-            <View style={{
-                width: '40%',
-                height: 200, backgroundColor: '#fff',
-                elevation: 8, borderRadius: 10,
-                marginTop: 330,
-                position: 'absolute',
-                zIndex: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-start',
-                marginLeft: 200,
+                <View style={styles.imageContainer}>
+                    <TouchableOpacity>
+                        <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
+                    </TouchableOpacity>
+                </View>
 
-            }}>
-                <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
-            </View>
-
-            <View style={{
-                width: '40%',
-                height: 200, backgroundColor: '#fff',
-                elevation: 8, borderRadius: 10,
-                marginTop: 330,
-                position: 'absolute',
-                zIndex: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-                alignSelf: 'flex-start',
-                marginLeft: 20,
-
-            }}>
-                <Icon name='add-box' size={55} color={'#ff739a'} style={{ zIndex: 5 }} />
             </View>
 
             {/* Entrar */}
 
-            <View style={{
-                width: '90%',
-                height: 50, backgroundColor: '',
+            <TouchableOpacity style={{
+                width: '60%',
+                height: 50,
+                backgroundColor: '#ff739a',
                 elevation: 8, borderRadius: 25,
-                marginTop: 565,
-                position: 'absolute',
-                zIndex: 0,
+                marginTop: 30,
                 justifyContent: 'center',
                 alignItems: 'center',
-                alignSelf: 'center'
 
             }}>
                 <Text style={{ fontSize: 15, fontWeight: 'bold', color: '#fff' }}>Aceitar termos e entrar</Text>
-            </View>
-
-
-            {/* Gradient */}
-
-            <View style={{
-                width: '100%',
-                height: '100%',
-                borderTopLeftRadius: 0,
-                alignItems: 'center'
-            }}>
-            </View>
-
-
+            </TouchableOpacity>
 
         </View>
     );
@@ -122,7 +74,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
     },
+
+    imageRow: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-evenly',
+        marginTop: 25,
+    },
+
+    imageContainer: {
+        width: '40%',
+        height: 200,
+        backgroundColor: '#fff',
+        elevation: 8,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 
 });
